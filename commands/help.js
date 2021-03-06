49ce6bc8476dce8a6a8dc8267837115ff76d215d
Mohammed Qubaisuddin <shareefhussain733@gmail.com>
2021-03-06 16:16:56 +0530

    PAB_assignment

--- commands/help.js
+++ commands/help.js
@@ -6,8 +6,8 @@ function helpfn(){
     console.log("wCat -n file_name                        =>give numbering to all the lines");
     console.log("wCat -b file_name                        =>give numbering to non_empty lines");
     console.log(`wCat "file_name1 > file_name2"           =>put all the content of file_name1 into file_name2 by overriding and also created file_name2 if not exists`);
-    console.log(`wCat "file_name1 >> file_name2"           =>append all the content of the file_name1 into file_name2`);
-    console.log(`wCat -s "file_name1 > file_name2"         =>get the file content of file_name1 and remove large line break spaces and save output in file_name2`);
+    console.log(`wCat "file_name1 >> file_name2"          =>append all the content of the file_name1 into file_name2`);
+    console.log(`wCat -s "file_name1 > file_name2"        =>get the file content of file_name1 and remove large line break spaces and save output in file_name2`);
     
 }
 
